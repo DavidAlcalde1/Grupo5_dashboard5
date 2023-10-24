f4d005f72f002f617f9799faa212d754d554fe96
unknown <david182_segura@hotmail.com>
2023-10-23 23:18:32 -0500

    Se agregaron las rutas en modulo de rutas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './App.css';
 import MenuHeader from './components/MenuHeader';
+import Users from "./pages/Users";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div>
-      <header>
-      
-      </header>
-      <main>
-        <MenuHeader/>
-      </main>
-    </div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="users" element={<Users />} />
+          <Route path="products" element={<Products />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
