fbd35c769e99419989ccf2cc702e7480cbdc397b
unknown <david182_segura@hotmail.com>
2023-10-27 20:44:58 -0500

    ya está consumiendo datos del api de users del node.js

--- src/components/pages/Users.js
+++ src/components/pages/Users.js
@@ -1,3 +1,32 @@
-export default function Users () {
-    return <div>Usuarios</div>
+import React, { useEffect, useState } from 'react';
+
+export default function UsersDashboard() {
+  const [users, setUsers] = useState([]);
+  const [ultimo, setUltimo] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/users')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data.data)
+        setUltimo (data.data[data.data.length-1])
+        console.log(data.data[data.data.length-1].idusers);
+      })
+      .catch((error) => console.error('Error al obtener usuarios:', error));
+  }, []);
+
+  return (
+    <div>
+      <h2>Listado de Usuarios</h2>
+      <h2>{users.length}</h2>
+      <ol>
+        {users.map((user) => (
+          <li key={user.idusers}>{user.first_name}</li>
+        ))}
+      </ol>
+      <label>{ultimo.idusers}</label> <br>
+      </br>
+      <label>{ultimo.first_name}</label>
+    </div>
+  );
 }
