664c061acf43e6b7a2a1ef5f37eb8a53dc303782
José David Alcalde Cabrera <david20egt@gmail.com>
2023-11-01 21:40:08 -0500

    Optimización de Código en Dashboard.js

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -19,22 +19,12 @@ export default function UsersDashboard() {
       .catch((error) => console.error('Error al obtener usuarios:', error));
   }, []);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/api/products')
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data.data);
-      })
-      .catch((error) => console.error('Error al obtener producto:', error));
-  }, []);
 
   useEffect(() => {
-    fetch('http://localhost:3000/api/products')
+    fetch(urlBase+'/api/products')
       .then((response) => response.json())
       .then((data) => {
-        setProducts(data.data);
-
-        // Contar la cantidad de registros por categoría
+        setProducts(data.data)
         const categoryCounts = {};
         data.data.forEach((product) => {
           const categoryId = product.idcategory;
